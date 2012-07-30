982414ce335e0afc8f46b6edca78d009f0e7b720
Juri <juri.strumpflohner@gmail.com>
2012-07-30 14:37:32 +0200

    added base setup

---
+++ src/Utf8FileConverter/Utf8Converter.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Utf8FileConverter
+{
+    class Utf8Converter
+    {
+        private string sourceDirectory;
+        private bool withoutBom = true;
+
+        public Utf8Converter(string sourceDirectory, bool withoutBom = true)
+        {
+            this.sourceDirectory = sourceDirectory;
+            this.withoutBom = withoutBom;
+        }
+    }
+}
